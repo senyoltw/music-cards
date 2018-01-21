a12da7596779b97664892ad183d70f651765653d
Thomas Schühly <thomas.schuehly@outlook.com>
2018-01-21 18:31:37 +0100

    First Iteration of local folder support

--- add_card.py
+++ add_card.py
@@ -7,7 +7,7 @@ cardList = CardList()
 while True:
         print 'Place the card in the reader'
         card = reader.readCard()
-        plist=raw_input('Specify Spotify URI, q to quit')
+        plist=raw_input('Specify Spotify URI or local folder path like this: file:/music/Card*/, q to quit')
         if plist=="q":
                 break
 	cardList.addPlaylist(card, plist)
